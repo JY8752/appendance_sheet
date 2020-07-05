319793a00dc90eb3df59040e5da40d5193d496d8
JY8752 <j.y.8752@gmail.com>
2020-07-05 11:25:28 +0900

    todo削除

--- src/main/java/com/example/attendance_sheet/Config/UserDetails/UserDetails.java
+++ src/main/java/com/example/attendance_sheet/Config/UserDetails/UserDetails.java
@@ -31,7 +31,6 @@ public class UserDetails implements org.springframework.security.core.userdetail
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -47,25 +46,21 @@ public class UserDetails implements org.springframework.security.core.userdetail
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
         return true;
     }
     
